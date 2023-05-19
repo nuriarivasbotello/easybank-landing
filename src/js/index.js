811f4fb164ec082942e787c83faf95b6e9e0472e
nuriarivasbotello <nuriarivasbotello@gmail.com>
2023-05-19 15:11:33 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
+
+const menuElement = document.getElementById("menu");
+const buttonElement = document.getElementById("toggle-menu");
+const menuIcon = document.getElementById("menu-icon");
+
+function handleClick() {
+  menuElement.classList.toggle("menu--show");
+
+  if (menuElement.classList.contains("menu--show")) {
+    menuIcon.src = "./assets/images/icon-close.svg";
+  } else {
+    menuIcon.src = "./assets/images/icon-hamburger.svg";
+  }
+}
+
+buttonElement.addEventListener("click", handleClick);
